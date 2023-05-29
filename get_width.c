08ce94fed5e0d0808bf277bb49a7aa267b3ef6fe
Daisy Soi <Soidaisy>
2023-05-29 09:29:27 -0700

    get_width.c

--- get_width.c
+++ get_width.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -11,10 +12,10 @@
 int get_width(const char *format, int *i, va_list list)
 {
 	int curr_i;
-	int width = 0
+	int width = 0;
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-	
+	{
 		if (is_digit(format[curr_i]))
 		{
 			width *= 10;
@@ -34,5 +35,4 @@ int get_width(const char *format, int *i, va_list list)
 
 	return (width);
 }
-
 
